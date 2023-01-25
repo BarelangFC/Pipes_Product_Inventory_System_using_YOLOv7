2cf5fe713112c22383c0d7be5e43b72eadbc7c11
jodimas-hotman <iwokk023@gmail.com>
2023-01-25 08:50:42 +0700

    first commit

--- calc_mAP.sh
+++ calc_mAP.sh
@@ -1,1 +1,1 @@
-./darknet detector map obj.data v7-tiny/v7-tiny.cfg v7-tiny/v7-tiny_best.weights
+./darknet detector map obj.data v7/v7.cfg v7/v7_best.weights -iou_thresh 0.50 -thresh 0.50 -points 0
